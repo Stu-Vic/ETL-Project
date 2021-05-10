79a760b74d61b61eccefe24aeb46fc87514a1ef0
kee-k <kaayin.kee@hotmail.com.au>
2021-05-10 23:01:43 +1000

    updated etl files inc deletions

--- table-schemata-updated.sql
+++ table-schemata-updated.sql
@@ -1,62 +1,54 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
-CREATE TABLE "accident_event" (
-    "accident_no" varchar   NOT NULL,
-	"event_type" varchar not null,
-    "event_type_desc" VARCHAR   NOT NULL,
-    "vehicle_1_coll_pt_desc" VARCHAR   NOT NULL,
-	"vehicle_2_coll_pt_desc" varchar not null,
-    PRIMARY KEY (
-        "accident_no"
-     )
-);
-
 CREATE TABLE "accident" (
     "accident_no" varchar   NOT NULL,
     "accident_date" varchar   NOT NULL,
     "accident_time" varchar   NOT NULL,
-    "accident_type_desc" VARCHAR   NOT NULL,
-    "day_week_desc" VARCHAR   NOT NULL,
-	"dca_code" varchar not null,
-    "dca_desc" VARCHAR   NOT NULL,
-    "node_ID" VARCHAR   NOT NULL,
-    "road_geometry_desc" VARCHAR   NOT NULL,
+    "accident_type_desc" varchar   NOT NULL,
+    "day_week_desc" varchar   NOT NULL,
+    "dca_code" varchar   NOT NULL,
+    "dca_desc" varchar   NOT NULL,
+    "node_id" varchar   NOT NULL,
+    "road_geometry_desc" varchar   NOT NULL,
     "speed_zone" varchar   NOT NULL,
-    CONSTRAINT "pk_accident_event" PRIMARY KEY (
+    CONSTRAINT "pk_accident" PRIMARY KEY (
         "accident_no"
      )
 );
 
 CREATE TABLE "accident_location" (
-    "accident_no" varchar,
-    "road_name" varchar,
-    "road_type" VARCHAR,
-    "road_name_int" VARCHAR,
-    "road_type_int" VARCHAR,
-    CONSTRAINT "pk_accident_location" PRIMARY KEY (
-        "accident_no"
-     )
+    "accident_no" varchar   NOT NULL,
+    "road_name" varchar   NOT NULL,
+    "road_type" varchar   NOT NULL,
+    "road_name_int" varchar   NOT NULL,
+    "road_type_int" varchar   NOT NULL
 );
 
--- ALTER TABLE "accident_event" ADD CONSTRAINT "fk_accident_event_accident_no_speed_zone" FOREIGN KEY("accident_no", "speed_zone")
--- REFERENCES "accident" ("accident_no", "speed_zone");
+CREATE TABLE "road_surface_cond" (
+    "accident_no" varchar   NOT NULL,
+    "surface_cond_desc" varchar   NOT NULL
+);
 
--- ALTER TABLE "accident_location" ADD CONSTRAINT "fk_accident_location_accdent_no_node_id" FOREIGN KEY("accdent_no", "node_id")
--- REFERENCES "accident" ("accident_no", "node_id");
+CREATE TABLE "atmospheric_cond" (
+    "accident_no" varchar   NOT NULL,
+    "atmosph_cond_desc" varchar   NOT NULL
+);
 
+ALTER TABLE "accident_location" ADD CONSTRAINT "fk_event_location_accident_no" FOREIGN KEY("accident_no")
+REFERENCES "accident" ("accident_no");
+
+ALTER TABLE "road_surface_cond" ADD CONSTRAINT "fk_road_surface_cond_accident_no" FOREIGN KEY("accident_no")
+REFERENCES "accident" ("accident_no");
+
+ALTER TABLE "atmospheric_cond" ADD CONSTRAINT "fk_atmospheric_cond_accident_no" FOREIGN KEY("accident_no")
+REFERENCES "accident" ("accident_no");
+
+-- drop table "accident";
+
+-- drop table "event_location"
 
 select * from accident;
 
-select * from accident_event;
-
 select * from accident_location;
 
-drop table accident;
+select * from atmospheric_cond;
 
-drop table accident_event;
-
-drop table accident_location;
-
--- NOTE!! accident_event table to be replaced
+select * from road_surface_cond;
